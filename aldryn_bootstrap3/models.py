47f904cd55f7fd16f27855fb50e99f22788ba2dc
Vladimir Nosov <vladimir@what.digital>
2020-05-05 15:26:01 +0700

    added on_delete

--- aldryn_bootstrap3/models.py
+++ aldryn_bootstrap3/models.py
@@ -1151,6 +1151,7 @@ class Bootstrap3CarouselSlideFolderPlugin(CMSPlugin):
     """
     folder = filer.fields.folder.FilerFolderField(
         verbose_name=_('Folder'),
+        on_delete=models.PROTECT
     )
     classes = model_fields.Classes()
 
